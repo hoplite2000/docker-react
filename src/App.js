ecd24601236fecda9c5092937b8ce9a95d381e67
Nehal N Shet <nehalnshet05@gmail.com>
2021-09-14 12:24:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Hello Docker!</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hub.docker.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Docker
         </a>
       </header>
     </div>
